e54642b72a7115ea2a514fe4f0eb1727720b2b18
John <john-root@users.noreply.github.com>
2018-08-07 16:41:55 +0100

    additional commenting

--- iris_data/iris_data_s3.py
+++ iris_data/iris_data_s3.py
@@ -15,19 +15,30 @@ class IrisSessionData:
         self.replacement_pattern = None
 
     def store_data(self, session_id, data):
-
+        """
+        store data for session
+        :param session_id: id for the session, should be uuid4
+        :param data: data to store
+        """
         try:
             self.client.put_object(Bucket=iris_settings.IRIS_SESSION_BUCKET, Key="session/" + session_id, Body=data)
         except ClientError:
             raise IrisStorageError("Error storing data")
 
     def store_json_data(self, session_id, json_data):
-
-        # TODO : comments for functions
+        """
+        store a python object that can serialize to json for the given session
+        :param session_id: id for the session, should be uuid4
+        :param json_data: a python object that can serialize to json
+        """
         self.store_data(session_id, json.dumps(json_data))
 
     def get_data(self, session_id):
-
+        """
+        get data for session
+        :param session_id: id for the session, should be uuid4
+        :return: byte[] of store data
+        """
         try:
             response = self.client.get_object(Bucket=iris_settings.IRIS_SESSION_BUCKET, Key="session/" + session_id)
             return response['Body'].read()
@@ -35,11 +46,20 @@ class IrisSessionData:
             raise IrisStorageError("Error getting data")
 
     def get_json_data(self, session_id):
-
+        """
+        get json data for session
+        :param session_id: id for the session, should be uuid4
+        :return: python object from deserialized json
+        """
         return json.loads(self.get_data(session_id))
 
     def store_shared_json_data(self, shared_id, json_data):
 
+        """
+        store a shared json blob under the given id which can be referenced from store json documents
+        :param shared_id: id for the shared data, should be uuid4
+        :param json_data: a python object that can serialize to json
+        """
         try:
             self.client.put_object(Bucket=iris_settings.IRIS_SESSION_BUCKET, Key="shared/" + shared_id,
                                    Body=json.dumps(json_data))
@@ -47,7 +67,11 @@ class IrisSessionData:
             raise IrisStorageError("Error storing data")
 
     def get_shared_json_data(self, shared_id):
-
+        """
+        retrieve a shared json blov for the given id
+        :param shared_id:  id for the shared data, should be uuid4
+        :return:  python object from deserialized json
+        """
         try:
             response = self.client.get_object(Bucket=iris_settings.IRIS_SESSION_BUCKET, Key="shared/" + shared_id)
             return json.loads(response['Body'].read())
@@ -56,6 +80,11 @@ class IrisSessionData:
 
     def expand_json_obj(self, obj):
 
+        """
+        expand the given object by looking for strings like [[<type>:<id>]] and replace with common or shared json
+        :param obj: python object from deserialized json
+        :return: expanded python object
+        """
         common = obj.get("common", [])
         return self.traverse_replace(obj, common)
 
